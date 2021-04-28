af5205f5acf95c3d12be2c38a13babf793e1850b
Izael F Menato Junior (Vulgo Menato) <menatinho@globomail.com>
2021-04-28 19:08:26 -0300

    crud vue js
    estudando vue js

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class id="app">
+    <div class="container grid-lg">
+      <header class="navbar">
+        <section class="navbar-section">
+          <router-link to="/" class="btn btn-link">Home</router-link>
+          <router-link to="/kanban" class="btn btn-link">Lista de Tarefas</router-link>
+        </section>
+        <section class="navbar-center">
+          <img class="img-responsive img-logo" src="@/assets/infnet.jpg" alt="Logo facu" />
+        </section>
+        <section class="navbar-section">
+          <a href class="btn btn-link">Twitter</a>
+          <a href class="btn btn-link">GitHub</a>
+        </section>
+      </header>
+      <router-view class="pt-2" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.img-logo {
+  max-width: 100px;
+}
+</style>
